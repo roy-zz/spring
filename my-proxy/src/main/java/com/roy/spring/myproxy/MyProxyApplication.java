56fa76a8bf9a2ec23fb9d92d23356614b43c901b
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-06-10 12:04:53 +0900

    my-proxy 모듈 추가

---
+++ my-proxy/src/main/java/com/roy/spring/myproxy/MyProxyApplication.java
@@ -0,0 +1,13 @@
+package com.roy.spring.myproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProxyApplication.class, args);
+    }
+
+}
---
+++ my-proxy/src/test/java/com/roy/spring/myproxy/MyProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.spring.myproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
